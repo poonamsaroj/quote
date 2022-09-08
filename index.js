f960306c242aeaec8c79a2c15d0b1a81cc4cd5ff
Poonam <sarojpoonam30@gmail.com>
2022-09-08 17:14:39 -0400

    added get call and ejs

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+var axios = require('axios');
+
+
+
+app.set("view engine", "ejs")
+
+app.get('/', (req, res, next) => {
+    axios.get('https://zenquotes.io/api/quotes')
+        .then(function (response) {
+            res.render("index", { quote: response.data[0]["q"], name: response.data[0]["a"] })
+        });
+})
+
+app.post('/', (req, res, next) => {
+    axios.get('https://zenquotes.io/api/quotes')
+        .then(function (response) {
+            res.send(response.data)
+        });
+})
+
+app.listen(3000);
